798e9568a3a605c06c1991e028dc907061e15ccd
SuttineeAtk <suttineeatk1998@gmail.com>
2022-08-19 14:36:11 +0700

    responsive

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 let menu = document.querySelector('.menu-icon');
+let navbar = document.querySelector(".navbar");
 
 menu.onclick = () => {
+    navbar.classList.toggle("open-menu");
     menu.classList.toggle("move")
-}
+};
+
+window.onscroll = () => {
+navbar.classList.remove("open-menu");
+menu.classList.remove("move");
+};
 
 // Education Swiper
 var swiper = new Swiper(".education-content", {
